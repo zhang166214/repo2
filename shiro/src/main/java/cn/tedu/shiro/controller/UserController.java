f5855d5d01cc3c5da7901468b6a40db31acb9063
zhang <zhang166214@163.com>
2020-08-19 14:02:06 +0800

    将shiro工程添加到版本库

---
+++ shiro/src/main/java/cn/tedu/shiro/controller/UserController.java
@@ -0,0 +1,15 @@
+package cn.tedu.shiro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+}
